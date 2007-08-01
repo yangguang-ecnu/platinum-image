9334fe49dc8592dc9200dcefeccfa9c583d3d5a6
arvid.r <arvid.r@d05cfb5e-bf2d-0410-8dd6-9f9931084c8a>
2007-08-01 19:38:39 +0000

    pageup/down stepping through image voxel by voxel

--- trunk/ platinum-image/renderer_base.cc
+++ trunk/ platinum-image/renderer_base.cc
@@ -20,13 +20,15 @@
 #include "renderer_base.h"
 
 int renderer_base::maxrendererID = 0;
+const float renderer_base::display_scale = 25;
+
 
 renderer_base::renderer_base()
     {
     maxrendererID++;
     identitet=maxrendererID;
 
-    std::cout << "renderer_base-kontruktör" << std::endl;
+    std::cout << "renderer_base-kontruktÃ¶r" << std::endl;
 
     imagestorender=NULL;
     imagestorender_id=0;
@@ -44,10 +46,12 @@ void renderer_base::connect_combination (rendercombination * c)
     imagestorender=c;
     imagestorender_id=c->get_id();
     }
+
 int renderer_base::combination_id()
     {
     return imagestorender_id;
     }
+
 void renderer_base::look_at(float x, float y, float z,float zoom)
     {
     wheretorender->look_at[0]=x;
@@ -58,48 +62,32 @@ void renderer_base::look_at(float x, float y, float z,float zoom)
         //0 indicates no selection
         {wheretorender->zoom=zoom;}
     }
-void renderer_base::move( float pan_x, float pan_y, float pan_z, float zoom_d)
-    {
-    //arguments are relative unit screen coordinates, ie. pan_x==1 moves wheretorender->look_at
-    //to the right the distance of image's longest edge
-    //also relative to view orientation, and pan_z is independent of zoom (which is intuitively right)
 
-    Vector3D pan,final_pan;
-
-    const bool use_constraints=true;
-
+void renderer_base::move( float pan_x, float pan_y, float pan_z)
+{
+    //arguments are physical coordinates,   
+    
+    Vector3D pan;
+    
+    /*const bool use_constraints=false;
+    
     const float min_zoom=0.5;
     const float max_zoom=10;
     float max_pan=0.5;           //in local coordinates; means panning to composite image edge
-
-    if ((wheretorender->zoom*zoom_d >= min_zoom && wheretorender->zoom*zoom_d <= max_zoom ) || !use_constraints)
-        {wheretorender->zoom*=zoom_d;}
-
+    
+    if ((wheretorender->zoom*zoom_d >= min_zoom && wheretorender->zoom*zoom_d <= max_zoom ) || !use_constraints)*/
+    
     pan.Fill(0);
-    pan[0]=pan_x/wheretorender->zoom;
-    pan[1]=pan_y/wheretorender->zoom;
-    pan[2]=pan_z; //ensures that z step is <= one voxel, not too elegant tho
-    //pan[2]=pan_z*voxel_resize[the_image][2][2]/data_max_norm[the_image]; //move the equivalent of 1 step in z-resolution
- 
-    wheretorender->look_at+=wheretorender->dir*pan;
-
-    if (use_constraints)
+    pan[0]=pan_x;
+    pan[1]=pan_y;
+    pan[2]=pan_z;
+    
+    wheretorender->look_at+=pan;
+    
+    /*if (use_constraints)
         {
         for (int d=0;d<3;d++)
             {
-            /*Vector3D test_pan;
-
-            test_pan.Fill(0);
-            test_pan[d]=pan[d]; //set test_pan vector to direction d component only
-
-            test_pan=wheretorender->look_at+wheretorender->dir*test_pan;
-
-            if (test_pan.GetNorm() > max_pan/wheretorender->zoom)   //ensure that panning the given
-            //distance in direction d won't take
-            //the visible area outside a box with
-            //side length max_pan*2
-            {pan[d]=0;}*/
-
             if (fabs(wheretorender->look_at[d]) > max_pan)                        
                 //if look_at is outside max_pan
                 //in any direction,
@@ -108,23 +96,42 @@ void renderer_base::move( float pan_x, float pan_y, float pan_z, float zoom_d)
                 {wheretorender->look_at[d]=max_pan*
                 wheretorender->look_at[d]/fabs(wheretorender->look_at[d]);}   
             }
-        }
-    }
+        }*/
+}
 
-void renderer_base::nudge (int x,int y,int z)
+void renderer_base::move_view (int vsize, int pan_x, int pan_y, int pan_z, float zoom_d)
+{
+    //also relative to view orientation
+    //zoom is a multiple of previous zoom value
+    
+    Vector3D pan;
+
+    pan.Fill(0);
+    pan[0]=pan_x*renderer_base::display_scale/(float)(vsize * wheretorender->zoom);
+    pan[1]=pan_y*renderer_base::display_scale/(float)(vsize * wheretorender->zoom);
+    pan[2]=pan_z;
+    
+    wheretorender->zoom*=zoom_d;
+    wheretorender->look_at+=wheretorender->dir*pan;
+}
+
+void renderer_base::move_voxels (int x,int y,int z)
 {
     Vector3D dir;
     dir[0] = x; dir[1] = y; dir[2] = z;
     
-    //combinations of images are rendered, here it is defined that
-    //nudge will move by the voxels of the topmost image
+    //combinations of images are rendered (which may have varying voxel sizes, 
+    //positions and orientation), here it is defined that
+    //move_voxels will move by the voxels of the topmost image
+    
     image_base* image = imagestorender->top_image();
     
-    //TODO: remove when world coordinate system is implemented
-        
-    //OTESTAT, KOLLA BERÄKNINGEN ELLER VÄNTA TILLS EFTER VÄRLDSKOORDINATERNA
-    dir = wheretorender->dir* image->direction * image->get_voxel_resize() * dir;
+    dir = wheretorender->dir*dir;
+    Matrix3D reOrient;
+    reOrient = image->get_orientation().GetInverse();
     
+    dir = reOrient* image->get_voxel_resize() * image->get_orientation() * dir;
+            
     move(dir[0],dir[1],dir[2]);
 }
 
@@ -154,10 +161,10 @@ void renderer_base::nudge (int x,int y,int z)
     }
     return v;*/
 
-std::map<std::string,float> renderer_base::get_values_world(Vector3D unitPos) const
+std::map<std::string,float> renderer_base::get_values_world(Vector3D worldPos) const
 {
     std::map<std::string,float> m;
-    Vector3D tlb;
+    /*Vector3D tlb;
     for (rendercombination::iterator itr = imagestorender->begin(); itr != imagestorender->end();itr++)
         {
         image_base * image = dynamic_cast<image_base *> (itr->pointer);
@@ -166,7 +173,7 @@ std::map<std::string,float> renderer_base::get_values_world(Vector3D unitPos) co
             {
             //center-based to top-left-back,
             //add particular image's geometric center
-            tlb=unitPos+image->unit_center();
+            tlb=worldPos+image->get_size()/2;
             tlb=image->unit_to_voxel()*tlb;
             if ( tlb[0] >= 0 && tlb[1] >= 0 && tlb[2] >= 0 && tlb[0] < image->get_size_by_dim(0) && tlb[1] < image->get_size_by_dim(1) && tlb[2] < image->get_size_by_dim(2))
                 {
@@ -175,6 +182,32 @@ std::map<std::string,float> renderer_base::get_values_world(Vector3D unitPos) co
             //else
             //    {m.push_back(-1);}
             }
+        }*/
+    
+    
+    for (rendercombination::iterator itr = imagestorender->begin(); itr != imagestorender->end();itr++)
+        {
+        image_base * image = dynamic_cast<image_base *> (itr->pointer);
+        
+        if (image != NULL)
+            {
+            Vector3D vPos = image->world_to_voxel(worldPos);
+            /*Vector3D size = image->get_size();
+            vPos +=  size/2;
+            Matrix3D pSize;
+            pSize = image->get_voxel_resize().GetInverse();
+            vPos = pSize * vPos;
+            Matrix3D dir = image->get_direction();
+            dir = dir.GetInverse();
+            vPos = dir * vPos;*/
+            
+            if ( vPos[0] >= 0 && vPos[1] >= 0 && vPos[2] >= 0 && vPos[0] < image->get_size_by_dim(0) && vPos[1] < image->get_size_by_dim(1) && vPos[2] < image->get_size_by_dim(2))
+                {
+                m[image->name()] = (image->get_number_voxel(vPos[0],vPos[1],vPos[2]));
+                }
+            
+            }
         }
+    
     return m;    
 }
