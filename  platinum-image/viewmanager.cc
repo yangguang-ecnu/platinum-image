d9b0bb3552462f9f3a90e457743cb321dcaa0895
joel.kullberg <joel.kullberg@d05cfb5e-bf2d-0410-8dd6-9f9931084c8a>
2009-06-26 08:46:40 +0000

    *Added rendergeometry_base class

---  platinum-image/viewmanager.cc
+++  platinum-image/viewmanager.cc
@@ -279,8 +279,8 @@ void viewmanager::setup_views(int virtualview, int areawidth, int areaheight) //
                 {
                 int VPind = find_viewport_index( viewportmapping[v][h][virtualview] );
                 //Create viewport widget and set position
-//                viewports[ VPind ].initialize_viewport(int(tilewidthpercentsum*areawidth),int(tileheightpercentsum*areaheight),tilewidth,tileheight);
-                viewports[ VPind ].initialize_viewport(int(tilewidthpercentsum*areawidth),int(tileheightpercentsum*areaheight),tilewidth,tileheight,PT_MIP);
+                viewports[ VPind ].initialize_viewport(int(tilewidthpercentsum*areawidth),int(tileheightpercentsum*areaheight),tilewidth,tileheight,PT_MPR);
+                //viewports[ VPind ].initialize_viewport(int(tilewidthpercentsum*areawidth),int(tileheightpercentsum*areaheight),tilewidth,tileheight,PT_MIP);
                 }
             else
                 {
